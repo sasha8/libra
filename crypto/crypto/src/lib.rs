ee827df7afa5579f1b0a4c1f9bc324cdc96afa9c
kphfb <kph@fb.com>
2020-06-07 21:23:15 -0700

    Update lib.rs

--- crypto/crypto/src/lib.rs
+++ crypto/crypto/src/lib.rs
@@ -22,7 +22,7 @@ mod unit_tests;
 pub use self::traits::*;
 pub use hash::HashValue;
 
-// Reexport once_cell and serde_name for use in CryptoHasher Derive implementation.
+// Re-export once_cell and serde_name for use in CryptoHasher Derive implementation.
 #[doc(hidden)]
 pub use once_cell as _once_cell;
 #[doc(hidden)]
