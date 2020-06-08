06a1ab4d3013b4873afa3e32c28cb0cf0b44b4f7
kphfb <kph@fb.com>
2020-06-07 21:28:10 -0700

    Update error.rs

--- crypto/crypto/src/error.rs
+++ crypto/crypto/src/error.rs
@@ -3,4 +3,6 @@
 
 //! Re-export the error types needed for the various crypto traits
 
+// REVIEW: Do we still need this file at all?  Seems like we could just import these in the files they are needed?
+
 pub use anyhow::{bail, Error};
